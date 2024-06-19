b0227b3532baff1980783267a8d19ed005eb89eb
Christos Tranoris <tranoris@ece.upatras.gr>
2024-06-19 12:16:59 +0300

    fixes to run tests in ide also. related to #41
    (cherry picked from commit 113bab7c99a2dc6600e4f6633423cec1f793cd1b)

--- src/test/java/org/etsi/osl/services/api/ResourceCatalogIntegrationTest.java
+++ src/test/java/org/etsi/osl/services/api/ResourceCatalogIntegrationTest.java
@@ -76,6 +76,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -95,7 +96,7 @@ import net.minidev.json.JSONObject;
 @RunWith(SpringRunner.class)
 @Transactional
 @SpringBootTest( webEnvironment = SpringBootTest.WebEnvironment.MOCK , classes = OpenAPISpringBoot.class)
-//@AutoConfigureTestDatabase //this automatically uses h2
+@AutoConfigureTestDatabase //this automatically uses h2
 @AutoConfigureMockMvc 
 @ActiveProfiles("testing")
 //@TestPropertySource(
