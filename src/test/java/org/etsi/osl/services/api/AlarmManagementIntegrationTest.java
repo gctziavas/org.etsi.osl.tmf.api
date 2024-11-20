53583e88431174718f4566f29186aec8c5717b42
Christos Tranoris <tranoris@ece.upatras.gr>
2024-11-20 12:58:01 +0200

    fix for #49

--- src/test/java/org/etsi/osl/services/api/AlarmManagementIntegrationTest.java
+++ src/test/java/org/etsi/osl/services/api/AlarmManagementIntegrationTest.java
@@ -54,6 +54,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -72,6 +73,7 @@ import org.springframework.web.context.WebApplicationContext;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = OpenAPISpringBoot.class)
 @AutoConfigureMockMvc
 @ActiveProfiles("testing")
+@AutoConfigureTestDatabase
 public class AlarmManagementIntegrationTest {
 
 	private static final transient Log logger = LogFactory.getLog(AlarmManagementIntegrationTest.class.getName());
