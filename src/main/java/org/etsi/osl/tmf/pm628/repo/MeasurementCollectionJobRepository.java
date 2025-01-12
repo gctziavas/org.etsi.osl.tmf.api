72fc080d28fb3ce82356d4a0536918a732240db9
Christos Tranoris <tranoris@ece.upatras.gr>
2025-01-12 10:02:39 +0200

    fixes for DB connection leaks for metrico transactions

--- src/main/java/org/etsi/osl/tmf/pm628/repo/MeasurementCollectionJobRepository.java
+++ src/main/java/org/etsi/osl/tmf/pm628/repo/MeasurementCollectionJobRepository.java
@@ -1,15 +1,13 @@
 package org.etsi.osl.tmf.pm628.repo;
 
+import java.util.Optional;
 import org.etsi.osl.tmf.pm628.model.ExecutionStateType;
 import org.etsi.osl.tmf.pm628.model.MeasurementCollectionJob;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface MeasurementCollectionJobRepository extends CrudRepository<MeasurementCollectionJob, Long>, PagingAndSortingRepository<MeasurementCollectionJob, Long> {
+public interface MeasurementCollectionJobRepository extends JpaRepository <MeasurementCollectionJob, Long> {
     Optional<MeasurementCollectionJob> findByUuid(String uuid);
 
     Iterable<MeasurementCollectionJob> findByExecutionState(ExecutionStateType executionState);
